ef35743f48550ed8fbe2e3d30d547f00e2a81e20
Pranay Raj Nagella <44527256+paranayrajnagella@users.noreply.github.com>
2021-04-24 20:29:10 +0530

    Update DateUtils.java

--- RestAssured/RestAssured/src/test/java/com/uitls/DateUtils.java
+++ RestAssured/RestAssured/src/test/java/com/uitls/DateUtils.java
@@ -6,10 +6,10 @@ import java.util.Date;
 
 public class DateUtils {
 	
-	//Hello adding some randomcommment
+	//Hello adding some randomcommmentHello
 	public static Date DateFormatter(String date,String format) throws ParseException
 	{ 
-			System.out.println(format+"create aconfilic");
+			System.out.println(format+"create aconfilic mammaiahoisafnfebkjrgbfiurbgrgbrigbreigbiugbgrii");
 			System.out.println("MY Code");
 		    Date date1=new SimpleDateFormat(format).parse(date);
 		    return date1;
