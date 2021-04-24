9c027b5c369f8fcc5df6b99ff3d6f6bfadfc192b
Pranay Raj Nagella <44527256+paranayrajnagella@users.noreply.github.com>
2021-04-24 20:20:06 +0530

    Update DateUtils.java

--- RestAssured/RestAssured/src/test/java/com/uitls/DateUtils.java
+++ RestAssured/RestAssured/src/test/java/com/uitls/DateUtils.java
@@ -6,9 +6,11 @@ import java.util.Date;
 
 public class DateUtils {
 	
+	//Hello adding some randomcommment
 	public static Date DateFormatter(String date,String format) throws ParseException
 	{ 
 			System.out.println(format);
+		System.out.println("MY Code")
 		    Date date1=new SimpleDateFormat(format).parse(date);
 		    return date1;
 	}
